94c1b10c524686715c7c38445044f4c025937226
Сергей Бердюг <sberdyug@gmail.com>
2017-05-07 02:19:08 +0300

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,37 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'bundler/setup'
+require 'mechanize'
+require 'date'
+require 'scraperwiki'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
+page = agent.get("http://pitchfork.com/reviews/albums/")
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+review_links = page.links_with(href: %r{^/reviews/albums/\w+})
+
+review_links = review_links.drop(1).reject do |link|
+  parent_classes = link.node.parent['class'].split
+  parent_classes.any? { |p| %w[next-container page-number].include?(p) }
+end
+
+reviews = review_links.map do |link|
+  review = link.click
+  artist = review.search('.artist-links').text
+  album = review.search('.review-title').text
+  label, year = review.search('.labels-and-years').text.split('•').map(&:strip)
+  reviewer = review.search('.display-name').text
+  review_date = DateTime.parse(review.search('.pub-date')[0]['title'])
+  score = review.search('.score').text.to_f
+  {
+    artist: artist,
+    album: album,
+    label: label,
+    year: year,
+    reviewer: reviewer,
+    review_date: review_date,
+    score: score
+  }
+end
+
+reviews.each do |review|
+  ScraperWiki.save_sqlite([:artist, :album], review)
+end
